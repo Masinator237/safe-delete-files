3674fec2cac8d22fe0ff0f41d94a343b3b128cd4
Masinator237 <49450371+Masinator237@users.noreply.github.com>
2019-10-01 16:02:36 +0000

    Uploaded project

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileErase
+{
+    class Handler
+    {
+       static private Random rnd;
+      static  Handler()
+        {
+            rnd = new Random();
+        }
+
+        public static byte[] random_bytes(int length)
+        {
+            length = length + 1;
+            byte[] random = new byte[length];
+            rnd.NextBytes(random);
+            return random;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Secure file erase sample");
+            Console.WriteLine("File ?");
+            string file = Console.ReadLine();
+            if (file != "")
+            {
+                File.WriteAllBytes(file, Handler.random_bytes(2048));
+                File.WriteAllBytes(file, Handler.random_bytes(4096));
+                File.WriteAllBytes(file, Handler.random_bytes(1024));
+                File.Delete(file);
+            }
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+        }
+    }
+}
